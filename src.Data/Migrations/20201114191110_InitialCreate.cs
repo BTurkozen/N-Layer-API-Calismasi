33f4856f8449a71f9b64abd45b815068c5ad655e
bkturkozen <bkturkozen@gmail.com>
2020-11-14 22:16:18 +0300

    Database işlemleri gerçekleştirildi(Connection string startup configuration'ı ve migration).

---
+++ src.Data/Migrations/20201114191110_InitialCreate.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace src.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    InnerBarcode = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "IsDeleted", "Name" },
+                values: new object[] { 1, false, "Meyveler" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "IsDeleted", "Name" },
+                values: new object[] { 2, false, "Sebzeler" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "InnerBarcode", "IsDeleted", "Name", "Price", "Stock" },
+                values: new object[,]
+                {
+                    { 1, 1, null, false, "ELma", 11.50m, 100 },
+                    { 2, 1, null, false, "Armut", 20.50m, 75 },
+                    { 3, 1, null, false, "Çilek", 10.0m, 50 },
+                    { 4, 1, null, false, "Ayva", 1.75m, 200 },
+                    { 5, 1, null, false, "Muz", 110.0m, 10 },
+                    { 6, 2, null, false, "Bamya", 30.50m, 25 },
+                    { 7, 2, null, false, "Pırasa", 5.0m, 17 },
+                    { 8, 2, null, false, "Ispanak", 20.0m, 88 },
+                    { 9, 2, null, false, "Brokoli", 12.50m, 122 },
+                    { 10, 2, null, false, "Lahana", 11.0m, 43 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
