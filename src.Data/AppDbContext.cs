07fad69f0170786bfdb3880d0741c0818e9d28aa
bkturkozen <bkturkozen@gmail.com>
2020-11-14 15:25:40 +0300

    Data Katmanı işlemleri: Context class'sı, Seed işlemleri, Entity'lerin Configuration property'leri oluşturuldu.

---
+++ src.Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using src.Core.Models;
+using src.Data.Configurations;
+using src.Data.Seeds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        //Tablolar oluşmadan çalışacak methodumuz.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Buradanda tanımlaya bilirdik ama buradaki kod satırlarını daha az tutabilmek ve
+            //Best practices için configuration classı oluşturarak entity'nin property'lerini orada tanımladık.
+            //modelBuilder.Entity<Product>().Property(x => x.Id).IsRequired();
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+
+            modelBuilder.ApplyConfiguration(new ProductSeed(new int[] { 1, 2 }));
+            modelBuilder.ApplyConfiguration(new CategorySeed(new int[] { 1, 2 }));
+        }
+    }
+}
