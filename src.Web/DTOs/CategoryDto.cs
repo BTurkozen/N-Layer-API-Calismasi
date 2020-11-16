459bfedd32f5389859f7c15208e6e09dd13d7adb
bkturkozen <bkturkozen@gmail.com>
2020-11-16 21:35:40 +0300

    Web-Layer işlemleri: MVC projesi oluşturdum, CategoryController oluşturup actionları(CRUD) ile birlikte view sayfalarıda oluşturdum, CategoryController için ServiceFilter oluşturup bunu DI ile enjekte ettik ve Controller'da kullandık.

---
+++ src.Web/DTOs/CategoryDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace src.Web.DTOs
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="{0} alanı boş olamaz."),Display(Name ="Category Name")]
+        public string Name { get; set; }
+    }
+}
---
+++ src.Web/DTOs/CategoryWithProductDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace src.Web.DTOs
+{
+    public class CategoryWithProductDto:CategoryDto
+    {
+        public ICollection<ProductDto> Products { get; set; }
+    }
+}
